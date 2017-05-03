9267f20c41a408f0e8f61ff67fa6fd374e3fb242
programadorB <programadorB@DESKTOP-2QVPDTR>
2017-05-03 21:15:25 +0200

    primer commit de programadorB

---
+++ src/proyectoprogramadora/ProgramadorB.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogramadora;
+
+/**
+ *esta clase la he creado después de pedir clonar el repositorio 
+ * @author programadorB
+ */
+public class ProgramadorB {
+    private String campo1;
+    private Integer campo2;
+
+    public ProgramadorB(String campo1, Integer campo2) {
+        this.campo1 = campo1;
+        this.campo2 = campo2;
+    }
+
+    public String getCampo1() {
+        return campo1;
+    }
+
+    public void setCampo1(String campo1) {
+        this.campo1 = campo1;
+    }
+
+    public Integer getCampo2() {
+        return campo2;
+    }
+
+    public void setCampo2(Integer campo2) {
+        this.campo2 = campo2;
+    }
+    
+}
