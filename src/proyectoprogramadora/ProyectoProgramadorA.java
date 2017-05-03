72e70271988c836032649898b5f7de2dc4f4eb37
programadorA <programadorA@DESKTOP-2QVPDTR>
2017-05-03 20:30:45 +0200

    PRIMER COMMIT DEL PROGRAMADORA

---
+++ src/proyectoprogramadora/ProyectoProgramadorA.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogramadora;
+
+/**
+ *YO HE COMENZADO A HACER EL PROYECTO. HE CREADO SÓLO ESTA CLASE
+ * HE INICIALIZADO EL REPOSITORIO GIT 
+ * HE HECHO UN COMMIT 
+ * HE HECHO PUSH AL REPOSITORIO GIT REMOTO https://github.com/informaticaMerche/proyectoprogramadoresABC.git
+ * 
+ * En GitHub yo soy el usuario programadorA-jm
+ * @author programadorA
+ */
+public class ProyectoProgramadorA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
